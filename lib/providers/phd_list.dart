455199793b46f8642eed6dc5df3089fe1e861655
yasht01 <yasht.2701@gmail.com>
2021-03-26 11:20:08 +0530

    Refactor /providers folder

--- lib/providers/phd_list.dart
+++ lib/providers/phd_list.dart
@@ -1,18 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
-// import 'package:mongo_dart/mongo_dart.dart';
-// import '../backend/server.dart' as server;
-import 'package:http/http.dart' as http;
-
-// Future<List<Aprof>> getAprof() async {
-//   final url = 'http://localhost:3000/listPage/Aprof';
-//   final response = await http.get(url);
-
-//   final jsonResponse = json.decode(response.body.toString());
-//   final dataList = AprofList.fromJson(jsonResponse['list']).list;
-//   return dataList;
-// }
 
 class Phd {
   final String userId;
--- lib/providers/prof_list.dart
+++ lib/providers/prof_list.dart
@@ -1,18 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
-// import 'package:mongo_dart/mongo_dart.dart';
-// import '../backend/server.dart' as server;
-// import 'package:http/http.dart' as http;
-
-// Future<List<Prof>> getAprof() async {
-//   final url = 'http://localhost:3000/listPage/prof';
-//   final response = await http.get(url);
-
-//   final jsonResponse = json.decode(response.body.toString());
-//   final dataList = ProfList.fromJson(jsonResponse['list']).list;
-//   return dataList;
-// }
 
 class Prof {
   final String userId;
